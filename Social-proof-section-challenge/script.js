986f5e4722b3ad0e42d34067d88e09ca15284400
Gian-P <75914587+Gian-P@users.noreply.github.com>
2022-02-07 14:57:39 -0400

    Delete Social-proof-section-challenge directory

--- Social-proof-section-challenge/script.js
+++
@@ -1,8 +0,0 @@
-let First_rating = document.querySelector(".first-5-stars");
-let Second_rating = document.querySelector(".second-5-stars");
-let Third_rating = document.querySelector(".third-5-stars");
-for(let i = 0; i < 5; i++){
-  First_rating.insertAdjacentHTML('afterbegin','<img src="./images/icon-star.svg" alt="">');
-  Second_rating.insertAdjacentHTML('afterbegin','<img src="./images/icon-star.svg" alt="">');
-  Third_rating.insertAdjacentHTML('afterbegin','<img src="./images/icon-star.svg" alt="">');
-}
